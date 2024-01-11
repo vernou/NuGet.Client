11089335e1f23847f29913799f23f62d309c8594
Jonatan Gonzalez (HE/HIM) (from Dev Box) <jongonza@microsoft.com>
2024-01-11 14:02:50 -0800

    Avoid creating empty lists

--- test/NuGet.Core.Tests/NuGet.ProjectModel.Test/JsonPackageSpecReaderTests.cs
+++ test/NuGet.Core.Tests/NuGet.ProjectModel.Test/JsonPackageSpecReaderTests.cs
@@ -3884,11 +3884,10 @@ namespace NuGet.ProjectModel.Test
                             {
                                 var frameworkPropertyName = reader.GetString();
                                 NuGetFramework framework = NuGetFramework.Parse(frameworkPropertyName);
-                                var dependencies = new List<LibraryDependency>();
 
                                 JsonPackageSpecReader.ReadCentralTransitiveDependencyGroup(
                                     jsonReader: ref reader,
-                                    results: dependencies,
+                                    results: out var dependencies,
                                     packageSpecPath: "SomePath");
                                 results.Add(new CentralTransitiveDependencyGroup(framework, dependencies));
                             }
@@ -3959,11 +3958,10 @@ namespace NuGet.ProjectModel.Test
                     {
                         reader.Read();
                         NuGetFramework framework = NuGetFramework.Parse(reader.GetString());
-                        var dependencies = new List<LibraryDependency>();
 
                         JsonPackageSpecReader.ReadCentralTransitiveDependencyGroup(
                             jsonReader: ref reader,
-                            results: dependencies,
+                            results: out var dependencies,
                             packageSpecPath: "SomePath");
                         results.Add(new CentralTransitiveDependencyGroup(framework, dependencies));
                     }
