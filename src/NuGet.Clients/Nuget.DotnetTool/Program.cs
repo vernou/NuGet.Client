66885270ff036e481489dd8b91efce38eae2106d
Jonatan Gonzalez (HE/HIM) (from Dev Box) <jongonza@microsoft.com>
2024-02-29 18:14:36 -0800

    Add build locator

--- src/NuGet.Clients/Nuget.DotnetTool/Program.cs
+++ src/NuGet.Clients/Nuget.DotnetTool/Program.cs
@@ -1,5 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using Microsoft.Build.Locator;
 
 namespace NuGet.DotnetTool
 {
@@ -7,6 +8,7 @@ namespace NuGet.DotnetTool
     {
         public static int Main(string[] args)
         {
+            MSBuildLocator.RegisterDefaults();
             return NuGet.CommandLine.XPlat.Program.MainInternal(args, "nugetcdk");
         }
     }
